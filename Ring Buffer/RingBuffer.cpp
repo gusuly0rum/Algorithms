7e76977acbfe88782d97f71c0f3c33b0798c1ad0
gusuly0rum <donguk.kim1112@gmail.com>
2018-10-06 19:51:43 -0700

    Start ring buffer

--- Ring Buffer/RingBuffer.cpp
+++ Ring Buffer/RingBuffer.cpp
@@ -1,1 +1,67 @@
+#include <iostream>
+#include <stdexcept>
 #include "RingBuffer.hpp"
+
+/* constructor */
+
+RingBuffer::RingBuffer() {
+  start = 0;
+  count = 0;
+  capacity = 5;
+  store = new int[capacity];
+  fill();
+}
+
+RingBuffer::~RingBuffer() {
+  delete[] store;
+}
+
+/* public */
+
+// O(1)
+int RingBuffer::get(int index) {
+  checkIndex(index);
+  return store[index];
+}
+
+void RingBuffer::push(int value) {
+  if (count == capacity) resize();
+  store[count] = value;
+  count++;
+}
+
+void RingBuffer::unshift(int value) {
+  if (count == capacity) resize();
+}
+
+void RingBuffer::print() {
+  std::cout << "{ ";
+  for (int index = 0; index < capacity; index++) {
+    std::cout << store[index];
+    if (index < capacity - 1) std::cout << ", ";
+  }
+  std::cout << " }\n" << std::endl;
+}
+
+/* private */
+
+void RingBuffer::checkIndex(int index) {
+  if (indexInvalid(index)) {
+    throw std::out_of_range("Index out of bounds");
+  }
+}
+
+bool RingBuffer::indexInvalid(int index) {
+  return (index < 0 ) || (index >= capacity);
+}
+
+void RingBuffer::fill() {
+  for (int index = 0; index < capacity; index++) {
+    store[index] = NULL;
+  }
+}
+
+void RingBuffer::resize() {
+  int new_capacity = capacity * 2;
+  int* new_store = new int[new_capacity];
+}
--- Ring Buffer/RingBuffer.hpp
+++ Ring Buffer/RingBuffer.hpp
@@ -1,5 +1,31 @@
 class RingBuffer {
+  // variables
+  int start;
+  int count;
+  int capacity;
+  int* store;
+  
 public:
+  // constructor & destructor
   RingBuffer();
+  ~RingBuffer();
+  
+  // accessors
+  int get(int index);
+  
+  // basic operators
+  int pop();
+  int shift();
+  void push(int value);
+  void unshift(int value);
+  
+  // debugger
+  void print();
+  
 private:
-}
+  // internal functions
+  void fill();
+  void resize();
+  void checkIndex(int index);
+  bool indexInvalid(int index);
+};
