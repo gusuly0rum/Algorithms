a1f96fc577e69005e6bc6b957c4182b589a5fc73
gusuly0rum <donguk.kim1112@gmail.com>
2018-10-08 15:20:45 -0700

    Do bst append

--- Binary Search Tree/BSTNode.cpp
+++ Binary Search Tree/BSTNode.cpp
@@ -11,4 +11,18 @@ bool BSTNode::unbound() {
 }
 
 void BSTNode::append(BSTNode* node) {
+  if (this->unbound()) {
+    if (node->value <= this->value) {
+      this->left = node;
+    } else {
+      this->rite = node;
+    }
+    
+  } else {
+    if (node->value <= this->value) {
+      this->left->append(node);
+    } else {
+      this->rite->append(node);
+    }
+  }
 }
