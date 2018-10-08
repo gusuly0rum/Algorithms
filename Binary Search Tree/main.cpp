e88dac8a3a5efba17b522809c6a3a72b03508831
gusuly0rum <donguk.kim1112@gmail.com>
2018-10-08 15:27:56 -0700

    Create test case in main

--- Binary Search Tree/main.cpp
+++ Binary Search Tree/main.cpp
@@ -3,6 +3,13 @@
 
 int main() {
   BinarySearchTree bst;
+  int values[9] { 5, 3, 7, 1, 4, 9, 0, 2, 10 };
+  int length = sizeof(values) / sizeof(values[0]);
+  
+  for (int index = 0; index < length; index++) {
+    bst.insert(values[index]);
+  }
+  
   std::cout << !nullptr << std::endl;
   return 0;
 }
