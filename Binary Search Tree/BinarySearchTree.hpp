2e07abfbe798f3f31caa63db1e769b1b902f98da
gusuly0rum <donguk.kim1112@gmail.com>
2018-10-08 12:19:56 -0700

    Start bst hpp file

--- Binary Search Tree/BinarySearchTree.hpp
+++ Binary Search Tree/BinarySearchTree.hpp
@@ -8,6 +8,12 @@ public:
 public:
   // constructor
   BinarySearchTree();
+  
+  // basic operations
+  void insert(int value);
+  void remove(int value);
+  bool includes(int value);
+  BSTNode* find(int value);
     
 private:
   // internal member functions
