f6f206421ddb250257de12cd9ab60c82085974ed
gusuly0rum <donguk.kim1112@gmail.com>
2018-10-08 17:35:46 -0700

    Create bfs and dfs projects

--- Breadth First Search/00_tree_node.rb
+++ Breadth First Search/00_tree_node.rb
@@ -7,44 +7,11 @@ class PolyTreeNode
     @children = [];
   end
 
-  def parent=(node)
-    @parent.children.delete(self) if @parent
-    @parent = node
-    if node && !node.include?(self)
-      node.children.push(self)
-    end
-  end
-
-  def add_child(node)
-    @children.push(node) unless include?(node)
-    node.parent = self
-  end
-
-  def remove_child(node)
-    raise 'node is not a child' unless include?(node)
-    @children.delete(node)
-    node.parent = nil
-  end
-
-  def include?(node)
-    @children.include?(node)
-  end
-
   def dfs(value)
     return self if self.value == value
     self.children.each do |child|
       node = child.dfs(value)
       return node if node
-    end
-    nil
-  end
-
-  def bfs(value)
-    queue = [self]
-    until queue.empty?
-      node = queue.shift
-      return node if node.value == value
-      node.children.each { |child| queue.push(child) }
     end
     nil
   end
---
+++ Breadth First Search/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Breadth First Search
+//
+//  Created by Donguk Kim on 10/8/18.
+//  Copyright © 2018 Donguk Kim. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
