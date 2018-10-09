4ff63dab4385f9f3aaba6ca3eab6b64adcda1f97
gusuly0rum <donguk.kim1112@gmail.com>
2018-10-08 18:12:15 -0700

    Complete bfs

--- Breadth First Search/BreadthFirstSearch.cpp
+++ Breadth First Search/BreadthFirstSearch.cpp
@@ -1,6 +1,6 @@
 #include "BreadthFirstSearch.hpp"
 
-std::vector<BSTNode*> bfs(BSTNode* root) {
+inline std::vector<BSTNode*> bfs(BSTNode* root) {
   BSTNode* node;
   std::vector<BSTNode*> children;
   std::vector<BSTNode*> queue { root };
@@ -9,12 +9,12 @@ std::vector<BSTNode*> bfs(BSTNode* root) {
   while (!queue.empty()) {
     node = queue[0];
     queue.erase(queue.begin());
-//    children = node->children();
+    children = node->children();
     
-    //    for (int index = 0; index < children.size(); index++) {
-    //      queue.push_back(children[index]);
-    //      order.push_back(children[index]);
-    //    }
+    for (int index = 0; index < children.size(); index++) {
+      queue.push_back(children[index]);
+      order.push_back(children[index]);
+    }
   }
   
   return order;
