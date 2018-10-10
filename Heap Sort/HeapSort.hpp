5ecb54ea93a5b408a18b72c1d4081b0399bcc8ee
gusuly0rum <donguk.kim1112@gmail.com>
2018-10-09 18:14:39 -0700

    Start heap sort

--- Heap Sort/HeapSort.hpp
+++ Heap Sort/HeapSort.hpp
@@ -1,1 +1,15 @@
+#include <vector>
+#include <iostream>
 
+class Heap {
+  // member variables
+  std::vector<int> store;
+  
+public:
+  // basic operations
+  void heapifyUp();
+  void heapifyDown();
+  
+private:
+  // internal members
+};
--- Heap Sort/main.cpp
+++ Heap Sort/main.cpp
@@ -1,4 +1,3 @@
-#include <iostream>
 #include "HeapSort.hpp"
 
 int main() {
