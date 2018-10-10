a8ab064a2fe931391637f534190d15ab1d1173c8
gusuly0rum <donguk.kim1112@gmail.com>
2018-10-09 20:23:31 -0700

    Start quicksort

--- Quick Sort/main.cpp
+++ Quick Sort/main.cpp
@@ -1,15 +1,5 @@
-//
-//  main.cpp
-//  Quick Sort
-//
-//  Created by Donguk Kim on 10/7/18.
-//  Copyright © 2018 Donguk Kim. All rights reserved.
-//
-
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+int main() {
+  return 0;
 }
