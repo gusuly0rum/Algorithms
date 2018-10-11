7075fe917650b3d2b51713c380c23e4add15c47f
gusuly0rum <donguk.kim1112@gmail.com>
2018-10-10 21:38:55 -0700

    Do naive quicksort

--- Quick Sort/Quicksort.cpp
+++ Quick Sort/Quicksort.cpp
@@ -1,1 +1,16 @@
 #include "Quicksort.hpp"
+
+void quicksort(std::vector<int> array) {
+  int length = (int) array.size();
+  int pivotIndex = length / 2;
+  int pivot = array[pivotIndex];
+  
+  int element;
+  std::vector<int> left;
+  std::vector<int> rite;
+  
+  for (int index = 0; index < length; index++) {
+    element = array[index];
+    element <= pivot ? left.push_back(element) : rite.push_back(element);
+  }
+}
