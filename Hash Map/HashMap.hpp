3bdb0a89971641a1db8e1e71fb4c41d703079cde
gusuly0rum <donguk.kim1112@gmail.com>
2018-10-15 13:20:04 -0700

    Start hashmap

--- Hash Map/HashMap.hpp
+++ Hash Map/HashMap.hpp
@@ -1,11 +1,25 @@
+#include "../Linked List/LinkedList.hpp"
+
 class HashMap {
   // member variables
+  int count;
+  int numBuckets;
+  LinkedList* store;
+  
 public:
   // constructor
   HashMap();
+  
   // accessors
+  int& operator[](int key);
+  
   // basic operations
+  void remove(int key);
+  bool includes(int key);
+  
   // debugger
+  
 private:
   // private members
+  void resize();
 };
