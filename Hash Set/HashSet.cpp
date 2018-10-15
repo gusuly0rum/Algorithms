c4b1756a9dc4f018bd8cdc0512f0557861699f3f
gusuly0rum <donguk.kim1112@gmail.com>
2018-10-15 12:46:08 -0700

    Do hashset

--- Hash Set/HashSet.cpp
+++ Hash Set/HashSet.cpp
@@ -1,4 +1,3 @@
-#include <array>
 #include <string>
 #include <sstream>
 #include <iostream>
@@ -9,14 +8,7 @@
 HashSet::HashSet() {
   this->count = 0;
   this->numBuckets = 5;
-  std::vector<int> temp[numBuckets];
-  this->store = temp;
-  std::cout << store[0].size() << std::endl;
-  std::cout << store[1].size() << std::endl;
-  std::cout << store[2].size() << std::endl;
-  std::cout << store[3].size() << std::endl;
-  std::cout << store[4].size() << std::endl;
-//  fill();
+  this->store = new std::vector<int>[numBuckets];
 }
 
 
@@ -24,16 +16,16 @@ HashSet::HashSet() {
 // O(1)
 void HashSet::insert(int value) {
   int internal = bucketIndex(value, numBuckets);
-  std::vector<int> bucket = store[internal];
-  bucket.push_back(value);
+  std::vector<int>* bucket = &store[internal];
+  bucket->push_back(value);
 }
 
 // O(1) but worst case O(k)
 void HashSet::remove(int value) {
   int internal = bucketIndex(value, numBuckets);
-  std::vector<int> bucket = store[internal];
-  int index = indexAt(bucket, value);
-  bucket.erase(bucket.begin() + index);
+  std::vector<int>* bucket = &store[internal];
+  int index = indexAt(*bucket, value);
+  bucket->erase(bucket->begin() + index);
 }
 
 // O(1) but worst case O(k)
@@ -49,32 +41,19 @@ bool HashSet::includes(int value) const {
 
 
 /* debugger */
-
-
 void HashSet::print() const {
-  std::cout << store[0].size() << std::endl;
-  std::cout << store[1].size() << std::endl;
-  std::cout << store[2].size() << std::endl;
-  std::cout << store[3].size() << std::endl;
-  std::cout << store[4].size() << std::endl;
+  std::cout << "\n{\n";
+  for (int indexRow = 0; indexRow < numBuckets; indexRow++) {
+    std::cout << "{ ";
+    for (int indexCol = 0; indexCol < store[indexRow].size(); indexCol++) {
+      std::cout << store[indexRow][indexCol];
+      if (indexCol < store[indexRow].size() - 1) std::cout << ", ";
+    }
+    std::cout << " }";
+    if (indexRow < numBuckets - 1) std::cout << ",\n";
+  }
+  std::cout << "\n}\n" << std::endl;
 }
-
-//void HashSet::print() const {
-//  int length;
-//  std::cout << "\n{\n";
-//  for (int indexRow = 0; indexRow < numBuckets; indexRow++) {
-//    std::cout << "{ ";
-//    length = (int) store[indexRow].size();
-//    std::cout << length << std::endl;
-//    for (int indexCol = 0; indexCol < length; indexCol++) {
-////      std::cout << store[indexRow][indexCol];
-//      if (indexCol < store[indexRow].size() - 1) std::cout << ", ";
-//    }
-//    std::cout << " }";
-//    if (indexRow < numBuckets - 1) std::cout << ",\n";
-//  }
-//  std::cout << "\n}\n" << std::endl;
-//}
 
 
 /* private */
@@ -96,13 +75,6 @@ int HashSet::indexAt(std::vector<int> bucket, int value) const {
     if (bucket[index] == value) return index;
   }
   return -1;
-}
-
-void HashSet::fill() {
-  for (int bucketIdx = 0; bucketIdx < numBuckets; bucketIdx++) {
-    store[bucketIdx].clear();
-    std::cout << "!" << std::endl;
-  }
 }
 
 void HashSet::resize() {
--- Hash Set/main.cpp
+++ Hash Set/main.cpp
@@ -3,11 +3,11 @@
 
 int main() {
   HashSet hashset;
-////  hashset.insert(1);
-////  hashset.insert(2);
-////  hashset.insert(3);
-////  hashset.insert(4);
-////  hashset.insert(5);
+  hashset.insert(1);
+  hashset.insert(2);
+  hashset.insert(3);
+  hashset.insert(4);
+  hashset.insert(5);
   hashset.print();
   
   return 0;
