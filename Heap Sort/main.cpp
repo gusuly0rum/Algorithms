7dc1f2bfaf5afce8f1bd2ab045d2923f56697bb2
gusuly0rum <donguk.kim1112@gmail.com>
2018-10-19 23:28:30 -0700

    start heapsort

--- Heap Sort/main.cpp
+++ Heap Sort/main.cpp
@@ -2,16 +2,20 @@
 
 int main() {
   
+  // test case BMH::insert
   BinaryMaxHeap bmh;
   for (int k = 0; k < 6; k++) {
     bmh.insert(k);
   }
 
+  // test case BMH::extract
   BinaryMaxHeap::print(bmh.store);
   bmh.extract();
   BinaryMaxHeap::print(bmh.store);
   bmh.extract();
   BinaryMaxHeap::print(bmh.store);
+  
+  // test case heapsort
   
   return 0;
 }
