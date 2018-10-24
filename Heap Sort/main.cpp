5276df6ef121e57d8c8fc4fe64421f384ccbe5a0
gusuly0rum <donguk.kim1112@gmail.com>
2018-10-23 17:05:49 -0700

    clean up main

--- Heap Sort/main.cpp
+++ Heap Sort/main.cpp
@@ -6,9 +6,7 @@ int main() {
   // test case BMH::insert
   BinaryMaxHeap bmh;
   std::vector<int> array { 0, 1, 2, 3, 4, 5 };
-  for (int k = 0; k < 6; k++) {
-    bmh.insert(k);
-  }
+  for (int k = 0; k < array.size(); k++) bmh.insert(array[k]);
 
   // test case BMH::extract
   BinaryMaxHeap::print(bmh.store);
