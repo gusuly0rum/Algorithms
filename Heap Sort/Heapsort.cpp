84a8c88a4ec7ea3a6585bd4c60cd53a29c9b4a69
gusuly0rum <donguk.kim1112@gmail.com>
2018-10-23 17:09:19 -0700

    add bmh alias for class

--- Heap Sort/Heapsort.cpp
+++ Heap Sort/Heapsort.cpp
@@ -1,9 +1,11 @@
 #include "Heapsort.hpp"
 #include "BinaryMaxHeap.hpp"
 
+using BMH = BinaryMaxHeap;
+
 // O(nlog(n))
 std::vector<int> heapsort(std::vector<int> array) {
-  BinaryMaxHeap heap;
+  BMH heap;
   std::vector<int> result;
   for (int k = 0; k < array.size(); k++) heap.insert(array[k]);
   while (heap.count() > 0) result.push_back(heap.extract());
@@ -12,7 +14,7 @@ std::vector<int> heapsort(std::vector<int> array) {
 
 std::vector<int> heapsortInplace(std::vector<int> array) {
   int count = (int) array.size();
-  for (int k = 0; k < count; k++) BinaryMaxHeap::heapifyUp(array, k);
+  for (int k = 0; k < count; k++) BMH::heapifyUp(array, k);
   
   int temp;
   while (count > 0) {
@@ -20,7 +22,7 @@ std::vector<int> heapsortInplace(std::vector<int> array) {
     temp = array[0];
     array[0] = array[count];
     array[count] = temp;
-    BinaryMaxHeap::heapifyDown(array, 0, count);
+    BMH::heapifyDown(array, 0, count);
   }
   
   return array;
