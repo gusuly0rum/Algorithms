0b9d53d4e1c68d75ec01a4b8118cda5bf01e327b
gusuly0rum <donguk.kim1112@gmail.com>
2018-10-25 00:45:34 -0700

    start graph

--- Graphs/Graph.hpp
+++ Graphs/Graph.hpp
@@ -1,1 +1,14 @@
-
+class Graph {
+  // member variables
+  
+public:
+  // constructor
+  Graph();
+  ~Graph();
+  
+  // basic operations
+  double density();
+  
+private:
+  // private members
+};
