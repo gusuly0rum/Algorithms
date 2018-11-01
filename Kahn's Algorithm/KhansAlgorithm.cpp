c3b5235eb7ff7c1bea84a68ed789df2d05ce9aa1
gusuly0rum <donguk.kim1112@gmail.com>
2018-11-01 15:45:26 -0700

    create kahns algorithm

---
+++ Kahn's Algorithm/KhansAlgorithm.cpp
@@ -0,0 +1,9 @@
+//
+//  KhansAlgorithm.cpp
+//  Kahn's Algorithm
+//
+//  Created by Donguk Kim on 11/1/18.
+//  Copyright © 2018 Donguk Kim. All rights reserved.
+//
+
+#include "KhansAlgorithm.hpp"
