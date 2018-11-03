25657fe94e079cfa646d55845b36918857de5fbf
gusuly0rum <donguk.kim1112@gmail.com>
2018-11-03 15:41:27 -0700

    do test case

--- Kahn's Algorithm/main.cpp
+++ Kahn's Algorithm/main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 #include "KhansAlgorithm.hpp"
 
+// debugger
+void vprint(std::vector<Node*>& array) {
+  std::cout << "{ ";
+  for (int index = 0; index < array.size(); index++) {
+    std::cout << array[index]->value;
+    if (index < array.size() - 1) std::cout << ", ";
+  }
+  std::cout << " }\n" << std::endl;
+}
+
+
 int main() {
   
   // test case
