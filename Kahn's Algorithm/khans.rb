f73af298985ba07b1d7c50eb17677fec5e6e37a9
gusuly0rum <donguk.kim1112@gmail.com>
2018-11-03 18:05:35 -0700

    update kahns.rb to have two methods

--- Kahn's Algorithm/khans.rb
+++ Kahn's Algorithm/khans.rb
@@ -1,12 +1,10 @@
 require '../Graphs/graph'
 
+# Method 1
 def topological_sort(nodes)
   stack = []
   result = []
-
-  nodes.each do |node|
-    stack.push(node) if node.in_edges.empty?
-  end
+  nodes.each { |node| stack.push(node) if node.in_edges.empty? }
 
   until stack.empty?
     node = stack.pop
@@ -19,4 +17,28 @@ def topological_sort(nodes)
     end
   end
   result.length == nodes.length ? result : []
+end
+
+
+# Method 2
+def topological_sort(nodes)
+  queue = []
+  sorted = []
+  num_in_edges = Hash.new(0)
+  nodes.each { |node| num_in_edges[node] = node.in_edges.length }
+  nodes.each { |node| queue.push(node) if node.in_edges.empty? }
+
+  until queue.empty?
+    node = queue.shift
+    sorted.push(node)
+
+    node.out_edges.each do |edge|
+      node = edge.to_vertex
+      num_in_edges[node] -= 1
+      if num_in_edges[node] == 0
+        queue.push(node)
+      end
+    end
+  end
+  sorted.length == nodes.length ? sorted : []
 end
