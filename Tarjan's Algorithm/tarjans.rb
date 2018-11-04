d9b5029e6cf2d124d8552382a60065733b5670fb
gusuly0rum <donguk.kim1112@gmail.com>
2018-11-04 13:55:34 -0800

    complete tarjans.rb cycle detection

--- Tarjan's Algorithm/tarjans.rb
+++ Tarjan's Algorithm/tarjans.rb
@@ -2,21 +2,28 @@ require '../Graphs/graph'
 
 def topological_sort(nodes)
   result = []
+  stack = Set.new
   visited = Set.new
   nodes.each do |node|
     unless visited.include?(node)
-      visit(node, visited, result)
+      cycle = visit(node, visited, stack, result)
+      return [] if cycle
     end
   end
   result
 end
 
-def visit(node, visited, result)
+def visit(node, visited, stack, result)
+  stack.add(node)
   visited.add(node)
   node.out_edges.each do |edge|
+    return true if stack.include?(edge.to_vertex)
     unless visited.include?(edge.to_vertex)
-      visit(edge.to_vertex, visited, result)
+      cycle = visit(edge.to_vertex, visited, stack, result)
+      return true if cycle
     end
   end
+  stack.delete(node)
   result.unshift(node)
+  return false
 end
